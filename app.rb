a156e87cb4aef4a179a44f74e79d96df191bfd1b
Charlie Cook <turingbombe@gmail.com>
2016-08-11 15:02:23 -0400

    Basic views from the couch: Done

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    set :views, "./views"
+    set :public_dir, "public"
+  end
 
+  get "/" do
+    erb :index
+  end
 end
